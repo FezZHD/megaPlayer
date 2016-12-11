bdea1080323c3823c5d98e6fd4199af9de4a6b57
FezZHD <fezzdev@gmail.com>
2016-12-11 22:20:39 +0300

    adding commands

---
+++ Player/Commands/SimpleCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Player.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+
+        private Action action;
+
+
+        public SimpleCommand(Action command)
+        {
+            action = command;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
